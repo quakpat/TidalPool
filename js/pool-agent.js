f00493ea5c440eeb22b2132d3329dcbb8aba9b06
quakpat <153359558+quakpat@users.noreply.github.com>
2025-01-17 16:29:40 +0100

    Update pool-agent.js

--- js/pool-agent.js
+++ js/pool-agent.js
@@ -4,7 +4,7 @@ export class PoolAgent {
             'https://rpc.helius.xyz/?api-key=b7b6ec9a-e258-4f73-ba77-429f2e0885f5',
             {
                 commitment: 'confirmed',
-                wsEndpoint: 'wss://rpc.helius.xyz/?api-key=YOUR_API_KEY'
+                wsEndpoint: 'wss://rpc.helius.xyz/?api-key=b7b6ec9a-e258-4f73-ba77-429f2e0885f5'
             }
         );
     }
